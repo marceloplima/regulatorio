e3845e4046729b238b0eb7d726fafffec6eb81bf
Marcelo Lima <marcelo.pires@druid.com.br>
2014-05-30 15:28:17 -0300

    Alteação para UFS

--- ssi-regulatorio/regulatorio-service/src/main/java/br/com/telefonica/ssi/regulatorio/service/UFServiceBean.java
+++ ssi-regulatorio/regulatorio-service/src/main/java/br/com/telefonica/ssi/regulatorio/service/UFServiceBean.java
@@ -1,11 +1,13 @@
 package br.com.telefonica.ssi.regulatorio.service;
 
+import javax.ejb.Stateless;
 import javax.persistence.TypedQuery;
 
 import br.com.telefonica.ssi.regulatorio.commom.domain.UF;
 import br.com.telefonica.ssi.regulatorio.commom.interfaces.UFService;
 import br.com.telefonica.ssi.service.AbstractCrudServiceBean;
 
+@Stateless
 public class UFServiceBean extends AbstractCrudServiceBean<UF, Integer> implements UFService{
 
 	/**
@@ -15,8 +17,8 @@ public class UFServiceBean extends AbstractCrudServiceBean<UF, Integer> implemen
 
 	@Override
 	public UF findByName(String name) {
-		TypedQuery<UF> q = getEntityManager().createQuery("select u from UF u wher upper(u.descricao) = :desc",UF.class);
-		q.setParameter("desc", name);
+		TypedQuery<UF> q = getEntityManager().createQuery("select u from UF u where upper(u.descricao) = :desc",UF.class);
+		q.setParameter("desc", name.toUpperCase());
 		UF uf = q.getSingleResult();
 		return uf;
 	}
