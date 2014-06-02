86366f136aac6a5c8e79eddee4de37bdf0572b6f
Marcelo Lima <marcelo.pires@druid.com.br>
2014-06-02 09:53:55 -0300

    Acerto interface

--- ssi-regulatorio/regulatorio-commom/src/main/java/br/com/telefonica/ssi/regulatorio/commom/interfaces/UFService.java
+++ ssi-regulatorio/regulatorio-commom/src/main/java/br/com/telefonica/ssi/regulatorio/commom/interfaces/UFService.java
@@ -1,10 +1,15 @@
 package br.com.telefonica.ssi.regulatorio.commom.interfaces;
 
+import javax.ejb.Local;
+
 import br.com.telefonica.ssi.regulatorio.commom.domain.UF;
 import br.com.telefonica.ssi.service.CrudService;
 
+@Local
 public interface UFService extends CrudService<UF, Integer>{
 
 	UF findByName(String name);
 
+	
+	
 }
