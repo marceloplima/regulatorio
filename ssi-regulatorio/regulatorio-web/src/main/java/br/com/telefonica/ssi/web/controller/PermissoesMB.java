472b797774dad976df760908442ccae9c58ca8b0
ltsiciliano <ltsiciliano@gmail.com>
2014-06-05 14:31:58 -0300

    Comitando novamente a última alteração a pedido do Marcelo

--- ssi-regulatorio/regulatorio-web/src/main/java/br/com/telefonica/ssi/web/controller/PermissoesMB.java
+++ ssi-regulatorio/regulatorio-web/src/main/java/br/com/telefonica/ssi/web/controller/PermissoesMB.java
@@ -33,6 +33,7 @@ public class PermissoesMB implements Serializable{
 	private List<Integer> statusPossiveis = new ArrayList<Integer>();
 	private List<Integer> statusNaoPossiveis = new ArrayList<Integer>();
 
+
 	private DemandasBean demandasmb = RecuperadorInstanciasBean.recuperarInstanciaDemandasBean();
 
 	@PostConstruct
